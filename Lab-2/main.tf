df92b36ad89090cdeff97b6c232e7ae4c35552ba
eimon <eimon.htun.tech@gmail.com>
2025-01-15 23:04:50 +0630

    Add Security Group ID

--- Lab-2/main.tf
+++ Lab-2/main.tf
@@ -6,6 +6,7 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami = "ami-06650ca7ed78ff6fa"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
     user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World" > index.html
