d8c0676f5877a4dcb823c790e62bf5cc4da5cfea
eimon <eimon.htun.tech@gmail.com>
2025-01-16 17:50:04 +0630

    Final of Lab-2

--- Lab-2/main.tf
+++ Lab-2/main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "example" {
     user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World" > index.html
-                nohup busybox httpd -f -p 8080 &
+                nohup busybox httpd -f -p ${var.server_port} &
                 EOF
 
 user_data_replace_on_change = true
@@ -23,9 +23,27 @@ Name = "terraform-example"
 resource "aws_security_group" "instance" {
 name = "terraform-example-instance"
 ingress {
-from_port = 8080
-to_port = 8080
+from_port = var.server_port
+to_port = var.server_port
 protocol = "tcp"
 cidr_blocks = ["0.0.0.0/0"]
 }
+
+tags = {
+    Name = "securitygroup_terraform"
+}
+}
+
+output "Public_IP" {
+  value = aws_instance.example.public_ip
+}
+
+output "vpc_security_group_ids" {
+  value = aws_security_group.instance.id
+}
+
+variable "server_port" {
+  description = "The Port the server will use for HTTP requests"
+  type = number
+  default = 8080
 }
