a3a0973a502c6de7febf8364dd2008868a4f4eea
eimon <eimon.htun.tech@gmail.com>
2025-01-16 18:24:14 +0630

    Create Auto-Scaling Group

---
+++ Lab-3/main.tf
@@ -0,0 +1,65 @@
+provider "aws" {
+  region  = "ap-southeast-1"
+  profile = "default"
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-06650ca7ed78ff6fa"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+  user_data       = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+
+  min_size = 2
+  max_size = 3
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "securitygroup_terraform"
+  }
+}
+
+variable "server_port" {
+  description = "The Port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
